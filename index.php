ce81b43d47567a9d47ef8be07d0b3cec158d9ea0
Otong23 <febryanando23@gmai.com>
2025-05-06 14:41:57 +0800

    ubah dari saya

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
 	<title>Coba push lagi</title>
 </head>
 <body>
-	<p>coba lagi</p>	
+	<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis temporibus at dolorem voluptates, voluptatem consequatur dolorum fugiat beatae pariatur nesciunt nulla! Autem, tenetur laboriosam nemo nulla asperiores animi illum culpa!
+	Officia nostrum dolorem veniam iusto eos sapiente excepturi laborum beatae aliquid! Eveniet consequatur ratione saepe eum, labore, fuga eos est, at architecto alias enim. Aliquam quis ducimus debitis in doloremque!</p>	
 	<ul>
 		<li>1</li>
 		<li>2</li>
